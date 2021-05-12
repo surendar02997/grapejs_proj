7c96922eb77cf91c06275f3c142c942a13d6d450
Your Name <you@example.com>
2021-05-12 19:16:48 +0530

    Borders changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import grapesjs from 'node_modules/grapesjs';
 import 'grapesjs-preset-newsletter';
+import 'grapesjs-firestore';
+
 
 @Component({
   selector: 'app-root',
@@ -11,19 +13,40 @@ import 'grapesjs-preset-newsletter';
 })
 
 export class AppComponent implements OnInit{
+
   editor:any;
   ngOnInit()
   {
+   
+   
+      const LandingPage = {
+      html: `<div>...</div>`,
+      css: null,
+      components: null,
+      style: null,
+    };
+
     this.editor = grapesjs.init({
 
       container: '#gjs',
 
-      // plugins: ['gjs-preset-newsletter'],
-      // pluginsOpts: {
-      //   'gjs-preset-newsletter': {
-      //     // options
-      //   }
-      // },
+      plugins: ['grapesjs-firestore'],
+      pluginsOpts: {
+        'grapesjs-firestore': {
+          docId: 'someID',
+          apiKey: "AIzaSyBAL7DnuotleQePI-FhYoquJ6eXCxebEjo",
+          authDomain: "angulartestapp-e2620.firebaseapp.com",
+          projectId: "angulartestapp-e2620",
+
+          databaseURL: "https://angulartestapp-e2620-default-rtdb.firebaseio.com",
+         
+          storageBucket: "angulartestapp-e2620.appspot.com",
+          messagingSenderId: "755391992083",
+          appId: "1:755391992083:web:f859e15bfc294064a18ae6",
+          measurementId: "G-1ZCEZJ2JGX"
+        }
+      },
+      
       
       // Get the content for the canvas directly from the element
       // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
@@ -31,7 +54,7 @@ export class AppComponent implements OnInit{
       // // Size of the editor
        height: '500px',
        width: 'auto',
-      //storageManager: false,
+     
       
 
       blockManager: {
@@ -50,125 +73,243 @@ export class AppComponent implements OnInit{
        },
        panels: { 
        
-       // defaults: [
-          // {
-          //   id: 'panel-switcher',
-          //   el: '.panel__switcher',
-          //   buttons: [{
-          //       id: 'show-layers',
-          //       active: true,
-          //       label: 'Layers',
-          //       command: 'show-layers',
-          //       // Once activated disable the possibility to turn it off
-          //       togglable: false,
-          //     }, {
-          //       id: 'show-style',
-          //       active: true,
-          //       label: 'Styles',
-          //       command: 'show-styles',
-          //       togglable: false,
-          //   }],
-          // }
-      //  ]  //this will disable all default panels
+      
       
       },
-      // selectorManager: {
-      //   appendTo: '.styles-container'
-      // },
-      // styleManager: {
-      //   appendTo: '.styles-container',
-      //   sectors: [{
-      //       name: 'Dimension',
-      //       open: false,
-      //       // Use built-in properties
-      //       buildProps: ['width', 'min-height', 'padding'],
-      //       // Use `properties` to define/override single property
-      //       properties: [
-      //         {
-      //           // Type of the input,
-      //           // options: integer | radio | select | color | slider | file | composite | stack
-      //           type: 'integer',
-      //           name: 'The width', // Label for the property
-      //           property: 'width', // CSS property (if buildProps contains it will be extended)
-      //           units: ['px', '%'], // Units, available only for 'integer' types
-      //           defaults: 'auto', // Default value
-      //           min: 0, // Min value, available only for 'integer' types
-      //         }
-      //       ]
-      //     },{
-      //       name: 'Extra',
-      //       open: false,
-      //       buildProps: ['background-color', 'box-shadow', 'custom-prop'],
-      //       properties: [
-      //         {
-      //           id: 'custom-prop',
-      //           name: 'Custom Label',
-      //           property: 'font-size',
-      //           type: 'select',
-      //           defaults: '32px',
-      //           // List of options, available only for 'select' and 'radio'  types
-      //           options: [
-      //             { value: '12px', name: 'Tiny' },
-      //             { value: '18px', name: 'Medium' },
-      //             { value: '32px', name: 'Big' },
-      //           ],
-      //        }
-      //       ]
-      //     }]
-      // },
+    
+
+      styleManager: {
+      //  // appendTo: '.styles-container',
+        sectors: [
+         {
+          name:'Typography',
+          open:false,
+          properties:[
+            {
+              name: "Vertical Align",
+              property: "vertical-align",
+              type: "select",
+              default: "auto",
+              list: [{
+                value: "auto",
+                name: "auto"
+            }, {
+                value: "top !important",
+                name: "top"
+            },
+            {
+                value: "middle !important",
+                name: "middle"
+            },
+            {
+                value: "bottom !important",
+                name: "bottom"
+            }
+        ]
+            },
+            {
+              name : 'Text Align',
+              property: 'text-align',
+              type: 'radio',
+              defaults: 'center',
+              list: [
+              { value : 'left', name : 'Left', className: 'fa fa-align-left'},
+              { value : 'center', name : 'Center', className: 'fa fa-align-center' },
+              { value : 'right', name : 'Right', className: 'fa fa-align-right'},
+              { value : 'justify', name : 'Justify', className: 'fa fa-align-justify'}
+              ],
+              },
+          ]
+         },
+          {
+            name: 'Dimension',
+            open: false,
+            // Use built-in properties
+            buildProps: ['width', 'min-height', 'padding','vertical-align'],
+            // Use `properties` to define/override single property
+            properties: [
+              {
+                // Type of the input,
+                // options: integer | radio | select | color | slider | file | composite | stack
+                type: 'integer',
+                name: 'The width', // Label for the property
+                property: 'width', // CSS property (if buildProps contains it will be extended)
+                units: ['px', '%'], // Units, available only for 'integer' types
+                defaults: 'auto', // Default value
+                min: 0, // Min value, available only for 'integer' types
+              }
+            ]
+          },
+          {
+            name: 'Extra',
+            open: false,
+            buildProps: ['background-color', 'box-shadow', 'custom-prop'],
+            properties: [
+              {
+                id: 'custom-prop',
+                name: 'Custom Label',
+                property: 'font-size',
+                type: 'select',
+                defaults: '32px',
+                // List of options, available only for 'select' and 'radio'  types
+                options: [
+                  { value: '12px', name: 'Tiny' },
+                  { value: '18px', name: 'Medium' },
+                  { value: '32px', name: 'Big' },
+                ],
+             },
+            
+            ]
+          },
+         
+          {
+            name: 'decoration',
+            buildProps: ['border'],
+            properties:[
+              {
+                name:'border-radius',
+                type: 'integer',
+                      units: ['px', 'em', 'rem'],
+                      property: 'border-radius',
+              },
+              {
+                      name: 'Border-bottom-Color',
+                      type: 'color',
+                      property: 'border-bottom-color',
+              },
+            ]
+          }
+          // {
+          //   name:'Decorations',
+          //   open:false,
+          //   buildProps: ['border-right-style','border-left-style','border-radius'],
+          //   properties:[
+              
+          //     {
+          //       name: 'Width',
+          //       type: 'integer',
+          //       units: ['px', 'em', 'rem'],
+          //       property: 'border-right-width',
+          //     },
+          //     {
+          //       name: 'Border left Width',
+          //       type: 'integer',
+          //       units: ['px', 'em', 'rem'],
+          //       property: 'border-left-width',
+          //     },
+          //     {
+          //       name: 'Style',
+          //       type: 'select',
+          //       property: 'border-right-style',
+          //       options: [
+          //         {value: 'none'},
+          //         {value: 'solid'},
+          //         {value: 'dotted'},
+          //         {value: 'dashed'},
+          //         {value: 'double'},
+          //         {value: 'groove'},
+          //         {value: 'ridge'},
+          //         {value: 'inset'},
+          //         {value: 'outset'}
+          //       ]
+          //     },
+          //     {
+          //       name: 'border left Style',
+          //       type: 'select',
+          //       property: 'border-left-style',
+          //       options: [
+          //         {value: 'none'},
+          //         {value: 'solid'},
+          //         {value: 'dotted'},
+          //         {value: 'dashed'},
+          //         {value: 'double'},
+          //         {value: 'groove'},
+          //         {value: 'ridge'},
+          //         {value: 'inset'},
+          //         {value: 'outset'}
+          //       ]
+          //     },
+          //     {
+          //       name: 'Color',
+          //       type: 'color',
+          //       property: 'border-right-color',
+          //     },
+          //     {
+          //       name: 'border left Color',
+          //       type: 'color',
+          //       property: 'border-left-color',
+          //     },
+              
+          //   ]
+          // }
+         
+       
+        ]
+      
+      },
+
+
 
 
       
       commands: {
         
         // options
-      }
+      },
+
+      storageManager: {
+    
+          type:'firestore',
+          autoload:true,
+          contentTypeJson: true,
+          setStepsBeforeSave: 1,
+          autosave: true,
+
+            //   autosave: false,
+            //   setStepsBeforeSave: 1,
+            //   type: 'remote',
+            // //  urlStore: 'http://cimailer.dev/templates/template',
+            // //  urlLoad: 'http://cimailer.dev/templates/template',
+            //   contentTypeJson: true,
+      },
       
-    })
+      
+    });
+
+    
         const blockManager = this.editor.BlockManager;
         const assetManager = this.editor.AssetManager;
         const panelManager = this.editor.Panels;
         const commands = this.editor.Commands;
 
-        // commands.add('myCommand', {
-        //   run(editor, sender) {
-        //     alert('Hello world!');
-        //   },
-        //   stop(editor, sender) {
-        //   },
-        // });
-
+ 
+       
          this.editor.getConfig().showDevices = false;  //to remove the show device from top
 
-        // this.editor.Commands.add('show-layers', {
-        //   getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
-        //   getLayersEl(row) { return row.querySelector('.layers-container') },
-        
-        //   run(editor, sender) {
-        //     const lmEl = this.getLayersEl(this.getRowEl(editor));
-        //     lmEl.style.display = '';
-        //   },
-        //   stop(editor, sender) {
-        //     const lmEl = this.getLayersEl(this.getRowEl(editor));
-        //     lmEl.style.display = 'none';
-        //   },
-        // });
-        // this.editor.Commands.add('show-styles', {
-        //   getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
-        //   getStyleEl(row) { return row.querySelector('.styles-container') },
-        
-        //   run(editor, sender) {
-        //     const smEl = this.getStyleEl(this.getRowEl(editor));
-        //     smEl.style.display = '';
-        //   },
-        //   stop(editor, sender) {
-        //     const smEl = this.getStyleEl(this.getRowEl(editor));
-        //     smEl.style.display = 'none';
-        //   },
-        // });
-        
+       //  this.editor.load(res => console.log('Load callback',res));
+       //  this.editor.store(res => console.log('Store callback',res));
+     
+       this.editor.on('storage:load', function(e) {
+        console.log('STORAGE:LOAD ', e);
+      });
+      this.editor.on('storage:store', function(e) {
+        console.log('STORAGE:STORE ', e);
+      });
+      this.editor.on('storage:error', function(e) {
+        console.log('STORAGE:ERROR ', e);
+      });
+      this.editor= this.editor;
 
-       
+      var html = this.editor.getHtml();
+      var css = this.editor.getCss();
+      console.log("html",html);
+      console.log("css",css);
+      
+
+      //firebase.firestore().settings({ experimentalForceLongPolling: true });
+
+
+       //For adding Assets
         assetManager.add({
           src: 'https://bit.ly/3vBrzTp',
           type: 'image',	//image is default
@@ -177,6 +318,24 @@ export class AppComponent implements OnInit{
         
         });
         
+
+        //For addin Blocks
+        blockManager.add('label', {
+          label: 'Label',
+          content: '<label>Put your Label here</label>',
+          category: 'Basic',
+          
+          attributes: {
+            
+            title: 'Insert Label block',
+          //  class:'fa fa-paragraph'
+           
+          },
+      
+        
+        
+        });
+
         blockManager.add('h1-block', {
         label: 'Heading',
         content: '<h1>Put your title here</h1>',
@@ -235,42 +394,38 @@ export class AppComponent implements OnInit{
             removable: false, // Once inserted it can't be removed
           }
         });
-      blockManager.add('link-replace', {
-        name: 'link-replace',
-        category: 'Extra',
-        label: 'Link Replace',
-        content: {
-          removable: true,
-          draggable: true,
-          droppable: true,
-          script: "console.log('the element', this)",
-          components: "<h2>Test Block</h2>",
-          traits: ['link-replace','name'],
-          style: {
-            width: '100px',
-            height: '100px'
-          }
-        },
-        attributes: {
-          title: 'Link Replace',
-          class: 'fa fa-external-link'
-        }
-      });
+  
 
         blockManager.add('the-row-block', {
           label: '2 Columns',
-          category: 'Basic',
           content: `
           <div class="row" data-gjs-droppable=".row-cell" data-gjs-custom-name="Row">
             <div class="row-cell" data-gjs-draggable=".row"></div>
             <div class="row-cell" data-gjs-draggable=".row"></div>
           </div>
-        
-        `,
-     //   content:{type:'cell'},
-        attributes:{class:'fa fa-columns'}
+
      
+
+          <style>
+            .row {
+              display: flex;
+              justify-content: flex-start;
+              align-items: stretch;
+              flex-wrap: nowrap;
+              padding: 10px;
+              min-height: 75px;
+            }
+            .row-cell {
+              flex-grow: 1;
+              flex-basis: 100%;
+              padding: 5px;
+            }
+        
+          </style>
+        `,
+        attributes:{class:'fa fa-columns'}
         });
+    
       
         blockManager.add('content-link', {
           label:'Youtube Link',
@@ -288,17 +443,19 @@ export class AppComponent implements OnInit{
           attributes: { 
             //class:'fa fa-youtube' 
           },
-          content:'<div><input class="form-control" type="text"></div>'
+          content:'<div><input class="form-control" type="text"></div>',
+          
 
-      })
+      });
 
 
 
        const block = blockManager.getAll();
-       console.log(JSON.stringify(block));
+      // console.log(JSON.stringify(block));
         
        
 
+       //For adding Panels
         panelManager.addPanel({
           id: 'panel-top',
          // el: '.panel__top',
@@ -339,14 +496,28 @@ export class AppComponent implements OnInit{
         });
 
         panelManager.addButton('options', [ 
-          { id: 'save', className: 'fa fa-floppy-o icon-blank', command: function(editor1, sender) { alert('save button clicked') }, 
+          { id: 'save', 
+          className: 'fa fa-floppy-o icon-blank', 
+          command:'save-db',
+          // command: function(editor1, sender) { 
+          //   if (sender)
+          //   sender.set('active', false);
+
+          // }, 
           attributes: { title: 'Save Template' } 
         }, 
       ]);
-        
-      //  const removedButton = panelManager.removeButton('myNewPanel', 'myNewButton');
-       // var asset = assetManager.getAllVisible();
-       // console.log(JSON.stringify(asset));
+    
+        this.editor.Commands.add
+        ('save-db', {
+            run: function(editor, sender)
+            {
+             // sender && sender.set('active'); // turn off the button
+             // editor.store();
+            }
+        });
+        // this.editor.on('storage:load', function(e) { console.log('Loaded ', e);});
+        // this.editor.on('storage:store', function(e) { console.log('Stored ', e);})
 
 
 
@@ -405,6 +576,24 @@ export class AppComponent implements OnInit{
         },
       },
   });
+
+  this.editor.DomComponents.addType('label', {
+    model: {
+      defaults: {
+        traits: [
+          'for', // Same as: { type: 'text', name: 'name' }
+          'id',
+          {
+            type: 'text',
+            name: 'For',
+            label: 'Label Name',
+          },
+        ]
+      }
+    }
+  });
+
+
 
 
   }
